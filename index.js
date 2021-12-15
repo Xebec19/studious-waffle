8d4ac47c5f45781cd68945192e1498cff9f1b1f1
xebec19 <rohant946@gmail.com>
2021-12-15 13:16:19 +0530

    add images in slide

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import pptxgen from "pptxgenjs";
 let pres = new pptxgen();
 
 // 2. Add a Slide to the presentation
-let slide = pres.addSlide();
-
+let slide1 = pres.addSlide("MASTER_NAME");
+let slide2 = pres.addSlide('Second Slide');
 // 3. Add 1+ objects (Tables, Shapes, etc.) to the Slide
-slide.addText("Hello World from PptxGenJS...", {
+slide2.addImage({path:"https://images.unsplash.com/photo-1578574577315-3fbeb0cecdc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",x:1,y:2}).addImage({path:"https://images.unsplash.com/photo-1578574577315-3fbeb0cecdc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",x:5,y:3});
+slide1.addText("Hello World from PptxGenJS...", {
     x: 1.5,
     y: 1.5,
     color: "363636",

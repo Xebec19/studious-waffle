54618a9008808a0def10aa99f2988e42ae10c694
xebec19 <rohant946@gmail.com>
2021-12-15 14:52:34 +0530

    ppds are easy

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ slide1.addText("Hello World from PptxGenJS...", {
     fill: { color: "F1F1F1" },
     align: pres.AlignH.center,
 });
-
+slide2.background = { color: "F1F1F1"};
+slide2.color = "696969";
+slide2.background = { path: "https://images.unsplash.com/photo-1578574577315-3fbeb0cecdc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"};
 // 4. Save the Presentation
 pres.writeFile({ fileName: "Sample Presentation.pptx" });
